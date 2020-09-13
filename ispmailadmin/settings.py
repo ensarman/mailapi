a5518d2d4080c193ad403d742f0835394a7a931a
Enrique Sardon <ensarman@gmail.com>
2020-09-13 17:24:51 -0500

    cambiando de posicion el autentication middleware

--- ispmailadmin/settings.py
+++ ispmailadmin/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'ispmailadmin.urls'
