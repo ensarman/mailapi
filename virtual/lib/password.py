a3971c4fcd1716e32ae85507a8556f4f104a0b38
Enrique Sardon <ensarman@gmail.com>
2020-09-13 20:13:05 -0500

    algo de documentacion

--- virtual/lib/password.py
+++ virtual/lib/password.py
@@ -5,9 +5,17 @@ from passlib.hash import bcrypt
 
 
 def crypt_pass(raw_pass):
-    """generate bcrypt password from a raw password"""
+    """
+    :param raw_pass: raw password
+    :return: bcrypt password from a raw password
+    """
     return '{BLF-CRYPT}' + bcrypt.using(rounds=12, ident='2y').hash(raw_pass)
 
 
 def pw_gen(size=15, chars=string.ascii_letters + string.digits):
+    """
+    :param size: size of the password
+    :param chars: complexity
+    :return: random password acording the given parameters
+    """
     return ''.join(random.choice(chars) for _ in range(size))
