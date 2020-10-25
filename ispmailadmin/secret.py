b699447209dee766cd74ca0382c44a9f948497ff
Enrique Sardon <ensarman@gmail.com>
2020-10-25 15:16:34 -0500

    eliminando algunos datos sensibles

--- ispmailadmin/secret.py
+++ ispmailadmin/secret.py
@@ -1,10 +1,10 @@
-SECRET_KEY = '%empt==-&ww@+6rz!&d@hg1&x&)&t$8i=p8smukftnbm+6#j$v'
+SECRET_KEY = ''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '172.20.0.1',
-        'NAME': 'mailserver',
-        'USER': 'root',
-        'PASSWORD': 'JE9YgqQ20uXOdFT8WP4BLHiBe1FpcK',
+        'HOST': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
