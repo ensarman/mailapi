6ced6cc267d9d27376538f6ca6b58dbb6e5c7fca
Enrique Sardon <ensarman@gmail.com>
2021-01-06 05:14:31 -0500

    preparando para produccion

--- ispmailadmin/settings.py
+++ ispmailadmin/settings.py
@@ -177,6 +177,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/ispmail_static'
 
 GRAPHENE = {
     'SCHEMA': 'ispmailadmin.schema.schema',
