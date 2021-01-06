0d7685dbad99fe6d22017464a88e64045dde6524
Enrique Sardon <ensarman@gmail.com>
2021-01-06 05:30:34 -0500

    colocando una buena ruta

--- ispmailadmin/settings.py
+++ ispmailadmin/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/ispmail_static'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 GRAPHENE = {
     'SCHEMA': 'ispmailadmin.schema.schema',
